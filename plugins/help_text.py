ab7e28efc5806ea247fb17b81a81ff403e5e1628
spilgt <35519589+spilgt@users.noreply.github.com>
2021-05-03 14:00:24 +0800

    Add files via upload

---
+++ plugins/help_text.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+from pyrogram import (
+    Client,
+    Filters,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS
+    return expires_at
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+          [
+            [
+              InlineKeyboardButton('💢 Channel 💢', url='https://telegram.me/fnprojects'),
+              InlineKeyboardButton('💢 Source 💢', url='https://github.com/FayasKallekkad/URL-Uploader-Bot')
+            ]
+          ]
+        ),
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+          [
+            [
+              InlineKeyboardButton('💢 Channel 💢', url='https://telegram.me/fnprojects'),
+              InlineKeyboardButton('💢 Source 💢', url='https://github.com/FayasKallekkad/URL-Uploader-Bot')
+            ]
+          ]
+        ),
+        reply_to_message_id=update.message_id
+    )
